a8a2136636b183921168c5b5cfd550851094130d
luckyPGY <luckybraziltube@gmail.com>
2024-12-23 12:03:22 -0300

    bad boy didnt update

--- Settings/domain.js
+++ Settings/domain.js
@@ -2,3 +2,8 @@
         const newUrl = window.location.pathname.replace(/\.html$/, '');
         window.history.replaceState(null, '', newUrl);
     }
+    if (window.location.pathname.endsWith('.htm')) {
+        const newUrl = window.location.pathname.replace(/\.htm$/, '');
+        window.history.replaceState(null, '', newUrl);
+    }
+
